dd133a39bc16f3981478a4cef989deab270237b4
Nnamdi Cyprian Ekechi <nnamdi4u09@gmail.com>
2024-01-20 13:48:26 +0100

    add link to view live and source code

--- app/projects/[uid]/page.tsx
+++ app/projects/[uid]/page.tsx
@@ -7,6 +7,10 @@ import { components } from "@/slices";
 import Bounded from "@/components/Bounded";
 import Heading from "@/components/Heading";
 import { formatDate } from "@/utils/FormatDate";
+import { PrismicNextLink } from "@prismicio/next";
+import { FaGithub } from "react-icons/fa6";
+import { MdArrowOutward } from "react-icons/md";
+import clsx from "clsx";
 
 type Params = { uid: string };
 
@@ -22,12 +26,49 @@ export default async function Page({ params }: { params: Params }) {
     <Bounded as="article">
       <div className="rounded-2xl border-2 border-slate-800 bg-slate-900 px-4 py-10 md:px-8 md:py-20">
         <Heading as="h1">{page.data.title}</Heading>
-        <div className="flex flex-wrap gap-2 ">
+        <div className="mt-2 flex flex-wrap gap-2">
           {page.tags.map((tag) => (
-            <span className="text-yellow-400 text-xl font-bold" key={tag}>{tag}</span>
+            <span className="text-[14px] font-bold text-yellow-400" key={tag}>
+              #{tag}
+            </span>
           ))}
         </div>
-        <p className="mt-8 border-b border-slate-600 text-xl font-medium text-slate-300">
+
+        <div className="mt-2 flex justify-between">
+          <PrismicNextLink
+            field={page.data.source_code}
+            className={clsx(
+              "group relative flex w-fit items-center justify-center overflow-hidden rounded-md border-2 border-slate-900 bg-slate-50 px-4  py-2 font-bold text-slate-800 transition-transform ease-out  hover:scale-105",
+            )}
+            aria-label="View source code"
+          >
+            <span
+              className={clsx(
+                "absolute inset-0 z-0 h-full translate-y-9 bg-yellow-300 transition-transform  duration-300 ease-in-out group-hover:translate-y-0",
+              )}
+            />
+            <span className="relative flex items-center justify-center gap-2">
+              Source Code <FaGithub className="inline-block" />
+            </span>
+          </PrismicNextLink>
+          <PrismicNextLink
+            field={page.data.source_code}
+            className={clsx(
+              "group relative flex w-fit items-center justify-center overflow-hidden rounded-md border-2 border-slate-900 bg-slate-50 px-4  py-2 font-bold text-slate-800 transition-transform ease-out  hover:scale-105",
+            )}
+            aria-label="View source code"
+          >
+            <span
+              className={clsx(
+                "absolute inset-0 z-0 h-full translate-y-9 bg-yellow-300 transition-transform  duration-300 ease-in-out group-hover:translate-y-0",
+              )}
+            />
+            <span className="relative flex items-center justify-center gap-2">
+              View Live <MdArrowOutward className="inline-block" />
+            </span>
+          </PrismicNextLink>
+        </div>
+        <p className="mt-5 border-b border-slate-600 text-sm font-medium text-slate-300">
           {formattedDate}
         </p>
         <div className="prose prose-lg prose-invert mt-12 w-full max-w-none md:mt-20">
