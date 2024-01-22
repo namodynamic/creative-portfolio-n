5eff71527a6a86fda860d38195d269a1d658d97a
Nnamdi Cyprian Ekechi <nnamdi4u09@gmail.com>
2024-01-22 11:49:50 +0100

    Add speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Urbanist } from "next/font/google";
 import "./globals.css";
 import clsx from "clsx";
@@ -36,6 +37,8 @@ export default function RootLayout({
         <div className="background-gradient absolute inset-0 -z-50 max-h-screen" />
         {/* <div className="pointer-events-none absolute inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-40 mix-blend-soft-light"></div> */}
         <Footer />
+        
+        <SpeedInsights />
       </body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
