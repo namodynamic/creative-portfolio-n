da4f776c909291a0f4b5de2d3772bf42e6a77491
Nnamdi Cyprian Ekechi <nnamdi4u09@gmail.com>
2024-01-22 09:40:06 +0100

    add view live link

--- app/projects/[uid]/page.tsx
+++ app/projects/[uid]/page.tsx
@@ -52,7 +52,7 @@ export default async function Page({ params }: { params: Params }) {
             </span>
           </PrismicNextLink>
           <PrismicNextLink
-            field={page.data.source_code}
+            field={page.data.view_live}
             className={clsx(
               "group relative flex w-fit items-center justify-center overflow-hidden rounded-md border-2 border-slate-900 bg-slate-50 px-4  py-2 font-bold text-slate-800 transition-transform ease-out  hover:scale-105",
             )}
