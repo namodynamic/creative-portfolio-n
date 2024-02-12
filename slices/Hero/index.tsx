4dd4d84c88061c7d8bb3106c92209f17126f2743
Nnamdi Cyprian Ekechi <nnamdi4u09@gmail.com>
2024-02-12 20:52:00 +0100

    Adjust text style

--- slices/Hero/index.tsx
+++ slices/Hero/index.tsx
@@ -87,7 +87,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
         <Shapes />
         <div className="col-start-1  md:row-start-1" data-speed=".2">
           <h1
-            className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter"
+            className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold whitespace-nowrap leading-none tracking-tighter"
             aria-label={
               slice.primary.first_name + " " + slice.primary.last_name
             }
@@ -96,7 +96,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
               {renderLetters(slice.primary.first_name, "first")}
             </span>
             <span className="-mt-[.2em] text-slate-500">
-              {renderLetters(slice.primary.last_name, "last")}
+                {renderLetters(slice.primary.last_name, "last")}
             </span>
           </h1>
           <span className="job-title block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-0 md:text-4xl">
