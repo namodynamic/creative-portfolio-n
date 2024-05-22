d686d98d8094cb97c5fbd6591deaffcb079cfc97
Nnamdi Cyprian Ekechi <nnamdi4u09@gmail.com>
2024-05-22 21:51:44 +0100

    Restore accidentally deleted file

--- slices/Hero/index.tsx
+++ slices/Hero/index.tsx
@@ -123,7 +123,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
         <div className="relative w-full overflow-hidden">
           <div className="relative z-10 my-20 flex justify-center">
             <div className="flex max-w-full flex-col items-center justify-center md:max-w-2xl lg:max-w-[60vw]">
-              <p className="intro max-w-80 text-center text-xs uppercase tracking-widest text-blue-100">
+              <p className="intro max-w-80 text-center text-xs uppercase tracking-tight text-blue-100">
                 {slice.primary.intro_text}
               </p>
 
@@ -132,7 +132,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
                 className="text-center text-[40px] md:text-5xl lg:text-6xl"
               />
 
-              <p className="mb-4 text-center text-sm text-blue-100 md:text-lg md:tracking-wider lg:text-2xl">
+              <p className="mb-4 text-center text-sm text-blue-100 md:text-lg md:tracking-wider tracking-tight lg:text-2xl">
                 {slice.primary.introduction}
               </p>
 
