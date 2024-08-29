e1cfb048dfc96835541befa0f645183021ae2f4b
Nnamdi Cyprian Ekechi <nnamdi4u09@gmail.com>
2024-08-29 22:27:56 +0100

    Adjust: css property

--- slices/Education/index.tsx
+++ slices/Education/index.tsx
@@ -17,21 +17,22 @@ const Education = ({ slice }: EducationProps): JSX.Element => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      <Heading as="h2" size="lg">
+      <Heading as="h2" size="lg" className="max-md:text-3xl">
         {slice.primary.heading}
       </Heading>
+
       {slice.items.map((item, index) => (
-        <div key={index} className="ml-6 mt-8 max-w-prose md:ml-12 md:mt-16">
-          <Heading as="h3" size="sm">
+        <div key={index} className="ml-6 mt-8 border-b md:ml-12 md:mt-16">
+          <Heading as="h3" size="sm" className="max-md:text-2xl">
             {item.title}
           </Heading>
 
-          <div className="mt-1 flex w-fit items-center gap-1 text-2xl font-semibold tracking-tight text-slate-400">
+          <div className="mt-1 flex w-fit items-center gap-1 text-2xl font-semibold tracking-tight text-slate-400 max-md:text-lg">
             <span>{item.time_period}</span>{" "}
-            <span className="text-3xl font-extralight">/</span>{" "}
+            <span className="text-3xl font-extralight max-md:text-xl">/</span>{" "}
             <span>{item.institution}</span>
           </div>
-          <div className="prose prose-sm prose-slate prose-invert col-start-1  mt-4 sm:prose-lg">
+          <div className="prose prose-sm prose-slate prose-invert col-start-1 mb-5 sm:prose-lg">
             <PrismicRichText field={item.description} />
           </div>
         </div>
