a6f35520b65ae7e6ad066356e7f0ce9af228d1b0
Nnamdi Ekechi <ekechinnamdi@gmail.com>
2024-12-23 21:56:58 +0100

    Remove whatsapp from footer social links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,6 @@ import { PrismicNextLink } from "@prismicio/next";
 import Bounded from "@/components/Bounded";
 import { isFilled } from "@prismicio/client";
 import { FaGithub, FaXTwitter, FaLinkedin } from "react-icons/fa6";
-import { FaWhatsapp, } from "react-icons/fa";
 import Link from "next/link";
 import { KeyTextField } from "@prismicio/client";
 
@@ -67,7 +66,7 @@ export default async function Footer() {
           {isFilled.link(settings.data.github_link) && (
             <PrismicNextLink
               field={settings.data.github_link}
-              className="p-2 text-2xl text-slate-300 transition-all duration-150 hover:scale-125 "
+              className="p-2 text-2xl text-slate-300 transition-all duration-150 hover:text-white "
               aria-label={settings.data.name + " on GitHub"}
             >
               <FaGithub />
@@ -76,25 +75,16 @@ export default async function Footer() {
           {isFilled.link(settings.data.twitter_link) && (
             <PrismicNextLink
               field={settings.data.twitter_link}
-              className="p-2 text-2xl text-slate-300 transition-all duration-150 hover:scale-125"
+              className="p-2 text-2xl text-slate-300 transition-all duration-150 hover:text-white"
               aria-label={settings.data.name + " on Twitter"}
             >
               <FaXTwitter />
             </PrismicNextLink>
           )}
-            {isFilled.link(settings.data.linkedin_link) && (
-              <PrismicNextLink
-                field={settings.data.whatsapp_link}
-                className="p-2 text-2xl text-slate-300 transition-all duration-150 hover:scale-125 "
-                aria-label={settings.data.name + " on WhatsApp"}
-              >
-                <FaWhatsapp />
-              </PrismicNextLink>
-            )}
           {isFilled.link(settings.data.linkedin_link) && (
             <PrismicNextLink
               field={settings.data.linkedin_link}
-              className="p-2 text-2xl text-slate-300 transition-all duration-150 hover:scale-125 "
+              className="p-2 text-2xl text-slate-300 transition-all duration-150 hover:text-white "
               aria-label={settings.data.name + " on LinkedIn"}
             >
               <FaLinkedin />
